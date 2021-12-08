7ce835a4aa3126cff53b38b8e7dcc064c27c5f33
BlackSwan-Decoded <56172407+BlackSwan-Decoded@users.noreply.github.com>
2021-12-08 14:30:35 -0500

    added a few chatbot rules along with the text_normalizer and contractions files from my NLP lectures

---
+++ rules.py
@@ -0,0 +1,46 @@
+utterance_response = [
+    [
+        r"my name is (.*)",
+        ["Hello %1, How are you today ?", ]
+    ],
+    [
+        r"hi|hey|hello",
+        ["Hello", "Hey there", ]
+    ],
+    [
+        r"what is your name ?",
+        ["My name is PokeBot! ", ]
+    ],
+    [
+        r"how are you ?",
+        ["I'm doing good.\nHow about You?", ]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright", "Its OK, never mind", ]
+    ],
+    [
+        r"I am fine",
+        ["Great to hear that, How can I help you?", ]
+    ],
+    [
+        r"i'm (.*) doing good",
+        ["Nice to hear that", "How can I help you? :)", ]
+    ],
+    [
+        r"(.*) age?",
+        ["I'm a computer program. I have no idea ;)", ]
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse", ]
+    ],
+    [
+        r"(.*) created ?",
+        ["Swanora created me using Python's NLTK library ", "top secret ;)", ]
+    ],
+    [
+        r"quit",
+        ["Bye take care. See you soon :) ", "It was nice talking to you. See you soon :)"]
+    ],
+]
